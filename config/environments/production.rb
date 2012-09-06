86a432ce16a8849f56ccc21636c08acde954f5f0
David Goodlad <david@goodlad.net>
2012-09-06 23:01:53 +1000

    Set asset host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ S3PhotoUploader::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
