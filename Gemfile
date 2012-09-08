c94a6e273fb29827540e42652b9d29ccf32635fd
David Goodlad <david@goodlad.net>
2012-09-08 16:50:40 +1000

    Who needs Rails?

--- Gemfile
+++ Gemfile
@@ -1,33 +1,13 @@
 source 'https://rubygems.org'
 
-gem 'rails', '3.2.6'
+gem 'sinatra', '~> 1.3.3'
 gem 'thin', '~> 1.4.1'
-
-gem 'pg', "~> 0.14.0"
 
 # Dev-only goodies
 group :development do
   gem 'heroku'
   gem 'foreman'
-  gem 'pry-rails'
+  gem 'pry'
 end
-
-# Gems used only for assets and not required
-# in production environments by default.
-group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
-
-  gem 'uglifier', '>= 1.0.3'
-
-  gem "asset_sync", "~> 0.4.2"
-
-  gem 'bootstrap-sass-rails', '~> 2.0.4'
-end
-
-gem "jquery-rails"
 
 gem "aws-sdk"
---
+++ app.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'aws'
+
+configure do
+  aws_config = AWS.config.with(
+    access_key_id:     ENV['PHOTOS_AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['PHOTOS_AWS_SECRET_ACCESS_KEY'],
+  )
+  set :aws_bucket, AWS::S3::Bucket.new(ENV['PHOTOS_BUCKET_NAME'], :config => aws_config)
+end
+
+get '/' do
+  erb :index
+end
+
+post '/sign' do
+  if params[:username] && params[:filename]
+    key = "#{params[:username]}/#{params[:filename]}"
+    presigned_post = AWS::S3::PresignedPost.new(
+      settings(:aws_bucket),
+      :key => key,
+      :secure => production?,
+      :content_type => "image/jpeg",
+      :content_length => 1..(3.megabytes),
+      :acl => "public-read"
+    )
+    Rack::Response.new(
+      JSON.generate("url" => presigned_post.url,
+                    "fields" => presigned_post.fields),
+      200,
+      { "Content-Type" => "application/json" }
+    ).finish
+  else
+    400 # Bad Request
+  end
+end
