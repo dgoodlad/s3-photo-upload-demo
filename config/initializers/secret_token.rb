d34639d031f89f358818a1e4cf7cf8a29bc46d48
David Goodlad <david@goodlad.net>
2012-09-09 10:29:00 +1000

    Kill off the last of the Rails bits

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-S3PhotoUploader::Application.config.secret_token = 'a89aad20fbe07fe03e93ed600f4d5d67ed8eb49e94eaa6a61e1fd1c09bc5793b097a9558e0ee2e5ab82789456759acc32e83465491cfbeba2ee0eb30355b9ce1'
